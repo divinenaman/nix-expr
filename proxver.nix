5cf3b0239c151af043f691c6c56dea5544f29c15
Naman Agarwal <naman.agarwal@juspay.in>
2023-10-18 18:23:17 +0530

    add devshell

--- proxver.nix
+++ proxver.nix
@@ -1,16 +1,17 @@
 { self, ... }:
 {
-  perSystem = { self', pkgs, ... } : let 
-    src = ./.;
-    in
+  perSystem = { self', pkgs, ... } :
     {
       packages.proxver = pkgs.writeShellApplication {
       name = "proxver";
       runtimeInputs = with pkgs; [ python311 ngrok ];
       text = ''
-          python -m http.server 4000 --directory ${src} &
+          python -m http.server 4000 &
           ngrok http --host-header=rewrite 4000
         '';
+      };
+      devShells.proxver = with pkgs; mkShell {
+        inputFrom = [ self'.packages.proxver ];
       };
     };
 }
