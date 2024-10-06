90842b535fac2329968ce3e532157ed50122a4ea
Naman Agarwal <namanddev7@gmail.com>
2024-10-06 23:05:30 +0530

    Fix lock file (#5)
    * update permissions on lock file
    
    * lock flake

--- flake.nix
+++ flake.nix
@@ -4,21 +4,21 @@
 	  	flake-utils.url = "github:numtide/flake-utils";
 	  	flake-parts.url = "github:hercules-ci/flake-parts";
 	  
-	  	myhello.url = "./flakes/hello-flake";
-	 	playwright-nix.url = "./flakes/playwright-nix";
-	  	python-app.url = "./python-app";
+	  	#myhello.url = "./flakes/hello-flake";
+	 	#playwright-nix.url = "./flakes/playwright-nix";
+	  	#python-app.url = "./python-app";
 	};
 
-	outputs = inputs@{ flake-parts, myhello, playwright-nix, python-app, ... }: 
+	outputs = inputs@{ flake-parts, ... }: 
 		flake-parts.lib.mkFlake { inherit inputs; } {
 			systems = [ "x86_64-linux" "aarch64-darwin" ];
-			imports = [ ./rust-build-package/module.nix ./flake-parts-modules/hello-module.nix ./flakes/playwright-nix/module.nix ./proxver.nix ];
-			perSystem = { pkgs, system, ... }: {
-				packages.default = pkgs.hello;
-				packages.subflake-myhello = myhello.packages.${system}.hello;
-				packages.subflake-playwright-test = playwright-nix.packages.${system}.playwright-test;
-				packages.python-app = python-app.packages.${system}.app;
-				devShells.python-app = python-app.devShell.${system};
-			};
+			imports = [ ./proxver.nix ];
+			#perSystem = { pkgs, system, ... }: {
+			#	packages.default = pkgs.hello;
+			#	packages.subflake-myhello = myhello.packages.${system}.hello;
+			#	packages.subflake-playwright-test = playwright-nix.packages.${system}.playwright-test;
+			#	packages.python-app = python-app.packages.${system}.app;
+			#	devShells.python-app = python-app.devShell.${system};
+			#};
 		};
 }
